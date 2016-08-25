48d3076e3d8811f083ebc9b22ef529a09737d223
Morgan Jones <morgan@larry.local>
2016-08-25 18:30:41 -0400

    updated for git 2.9.3, removed old script

--- create_git_repo.sh
+++ create_git_repo.sh
@@ -1,42 +1,32 @@
 #!/bin/sh
 #
-# abandoned in favor of setup_git_repo.sh
+# assumption:
+# mkdir /path
+# chgrp groupname /path
+# chmod 2770 /path
+#
+# example usage:
+# create_git_repo.sh /git/techops/repo sso-admin@domain.tld
 
-git_dir=/git
-
-if [ "x" == "x$3" ]; then
-    echo "usage: $0 <group name> <repo name> <repo description>"
+if [ "x" == "x$2" ]; then
+    echo "usage: $0 /path/to/repo <email addr>"
     exit
 fi
 
-group=$1
-repo=$2
-desc=$3
+path=$1
+email=$2
+repo_name=`basename $path`
 
-if [ `whoami` != "root" ]; then
-    echo "run as root!"
+mkdir $path
+if [ $? -ne 0 ]; then
     exit
 fi
 
-if [ `getent group|cut -d: -f1|egrep "^${group}$"` != "$1" ]; then
-    echo $group is not a valid unix group
-    exit
-fi
+cd $path
+git init --bare
 
-umask 002
-cd /git
-if [ -d $group ]; then
-    cd $group
-fi
-
-mkdir $repo
-chgrp $group $repo
-chmod 2770 $repo
-
-cd $repo
-git --bare init
-ln -s /usr/share/git-core/contrib/hooks/post-receive-email hooks/post-receive
-echo $desc > description
-git config hooks.mailinglist ldap-admin@philasd.org
-git config hooks.emailprefix "[git] "
-git repo-config core.sharedRepository true
+ln -s /usr/local/share/git-core/contrib/hooks/git_multimail.py hooks/post-receive
+git config multimailhook.commitlist ''
+git config multimailhook.emailprefix "[git] ${repo_name} "
+git config multimailhook.mailingList $email
+git config core.sharedRepository true
